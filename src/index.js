9c94f7e1e8fa8a31e549072d1cbb26992267d39f
Abhishek Mohanty <51206988+abhishekm2106@users.noreply.github.com>
2023-08-05 22:06:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let server =  config.mongoose.url;
 
 mongoose.connect(server)
 .then(()=>console.log("db connected successfully"))
-.catch(()=>console.log("db connection failed"))
+.catch((e)=>console.log(e))
 app.listen(config.port,()=>
     console.log("server started on port : ",config.port))
 // TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
